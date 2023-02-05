91e448949f96857de8d7da9c4ca8b4ce3d62d73c
conorgouraud <60387223+conorgouraud@users.noreply.github.com>
2023-02-05 17:21:04 +0100

    sobol stuff

--- Sobol_Run.py
+++ Sobol_Run.py
@@ -1,5 +1,4 @@
 from itertools import combinations
-
 import matplotlib.pyplot as plt
 from IPython.display import clear_output
 import SALib
@@ -8,24 +7,26 @@ from SALib.analyze import sobol
 import pandas as pd
 import geo_model2
 from geo_model2 import *
+from Sobol_input_generator import *
 import numpy as np
 import time
 
-samples = pd.read_csv("Sobol_256.csv", index_col=0)
+samples = pd.read_csv("Sobol_inputs.csv", index_col=0)
 # samples = samples[:51]      # PAUL
 # samples = samples[51:102]   # SOUVIK
 # samples = samples[102:153]  # TIJN
-# samples = samples[153:204]  # CONOR
+# samples = samples[153:159]  # CONOR #204
 # samples = samples[204:]     # GAIA
+
+# print(samples)
 
 # TODO slice your region here before removing the KeyboardIntterupt
 
-raise KeyboardInterrupt
-#just testing
+#raise KeyboardInterrupt
 
-print(samples)
 avg_last_welfare = []
-avg_last_price = []
+gini_list = []
+modularity_list = []
 
 for i in range(len(samples)):
     new = geo_model2.GeoModel(cost_clean=samples.iloc[i][0],
@@ -36,45 +37,47 @@ for i in range(len(samples)):
                              metabolism_scalar_money= samples.iloc[i][5],
                              eta_global_trade= samples.iloc[i][6],
                              predisposition_decrease= samples.iloc[i][7])
-    new.run_model(1000)
+
+    steps = 5
+    new.run_model(steps)
+
     nw1 = new.datacollector.get_agent_vars_dataframe()
     nw2 = new.datacollector.get_model_vars_dataframe()
-    # print(nw2)
 
-    #nw2 = new.datacollector.get_model_vars_dataframe()
+    last_gini = nw2["Gini_welfare"][steps]
+    gini_list.append(last_gini)
+
+    last_modularity = nw2["modularity_ga"][steps]
+    modularity_list.append(last_modularity)
+
     df_by_country_welfare = nw1.pivot_table(values = 'Welfare', columns = 'AgentID', index = 'Step')
-    # df_by_country_price = nw2.pivot_table(values = 'Price', columns = 'AgentID', index = 'Step')
+    df_by_country_gini = nw1.pivot_table(values='Welfare', columns='AgentID', index='Step')
+    df_by_country_mod = nw1.pivot_table(values='Welfare', columns='AgentID', index='Step')
 
-    # print(df_by_country_price)
-    #last_state = df_by_country.iloc[-1]
     avg_last_welfare.append(np.mean(df_by_country_welfare.iloc[-1]))
-    avg_last_price.append(nw2.iloc[-1][0])
 
-    #print(np.mean(avg_last_welfare))
-#print(len(avg_last_price))
-
-outputs1 = pd.DataFrame(data = avg_last_welfare,
+output_welfare = pd.DataFrame(data = avg_last_welfare,
         columns = ['output_welfare'])
 
-outputs2 = pd.DataFrame(data = avg_last_price,
-        columns = ['output_price'])
+output_gini = pd.DataFrame(data = gini_list,
+        columns = ['gini'])
+
+output_modularity = pd.DataFrame(data = modularity_list,
+        columns = ['modularity'])
+
 
 # TODO SAVE MY DATA BITTE
 
-outputs1.to_csv("paul1.csv")
-outputs2.to_csv("paul2.csv")
+#raise KeyboardInterrupt
 
-
-raise KeyboardInterrupt
-
-# print(outputs1)
-# print(outputs2)
-
-S_i_welfare = sobol.analyze(problem, outputs1['output_welfare'].values, print_to_console=True, calc_second_order=False)
+S_i_welfare = sobol.analyze(problem, output_welfare['output_welfare'].values, print_to_console=True, calc_second_order=False)
 #print(S_i_welfare)
 
-S_i_price = sobol.analyze(problem, outputs2['output_price'].values, print_to_console=True, calc_second_order=False)
+S_i_gini = sobol.analyze(problem, output_gini['gini'].values, print_to_console=True, calc_second_order=False)
 #print(S_i_price)
+
+S_i_modularity = sobol.analyze(problem, output_modularity['modularity'].values, print_to_console=True, calc_second_order=False)
+#print(S_i_modularity)
 
 def plot_index(s, params, i, title=''):
     """
@@ -107,29 +110,33 @@ def plot_index(s, params, i, title=''):
     plt.title(title)
     plt.ylim([-0.2, len(indices) - 1 + 0.2])
     plt.yticks(range(l), params)
-    plt.errorbar(indices, range(l), xerr=errors, linestyle='None', marker='o')
+    plt.errorbar(indices, range(l), color = 'k', ecolor = 'b', xerr=errors, linestyle='None', marker='o', capsize = 3, linewidth = 1)
     plt.axvline(0, c='k')
 plt.figure()
+
 for Si in [S_i_welfare]:
     # First order
     plot_index(Si, problem['names'], '1', 'Welfare First order sensitivity')
-    # plt.show()
     # Second order
     # plot_index(Si, problem['names'], '2', 'Second order sensitivity')
-    #     plt.show()
     # Total order
     plot_index(Si, problem['names'], 'T', 'Welfare Total order sensitivity')
-    #plt.show()
+plt.show()
 
+for Si in [S_i_gini]:
+    # First order
+    plot_index(Si, problem['names'], '1', 'Gini First order sensitivity')
+    # Second order
+    # plot_index(Si, problem['names'], '2', 'Second order sensitivity')
+    # Total order
+    plot_index(Si, problem['names'], 'T', 'Gini Total order sensitivity')
+plt.show()
 
-# for S in [S_i_price]:
-#     # First order
-#     plot_index(S, problem['names'], '1', 'Price First order sensitivity')
-#     # plt.show()
-#     # Second order
-#     # plot_index(S, problem['names'], '2', 'Second order sensitivity')
-#     #     plt.show()
-#     # Total order
-#     plot_index(S, problem['names'], 'T', 'Price Total order sensitivity')
-print(time.time() - now)
+for Si in [S_i_modularity]:
+    # First order
+    plot_index(Si, problem['names'], '1', 'Modularity First order sensitivity')
+    # Second order
+    # plot_index(Si, problem['names'], '2', 'Second order sensitivity')
+    # Total order
+    plot_index(Si, problem['names'], 'T', 'Modularity Total order sensitivity')
 plt.show()
---
+++ Sobol_input_generator.py
@@ -0,0 +1,40 @@
+from SALib.sample import saltelli
+import numpy as np
+from itertools import combinations
+import pandas as pd
+
+problem = {
+    'num_vars': 8,
+    'names': [
+     'cost_clean',
+     'cost_dirty',
+     'base_output_dirty',
+     'base_output_clean',
+     'metabolism_scalar_energy',
+     'metabolism_scalar_money',
+     'eta_global_trade',
+    'predisposition_decrease'],
+     'bounds': [[0.001,  1], [0.001,  1], [0.001,  1], [0.001,  1], [0.001, 2], [0.001, 2], [0.001, 1], [0.001, 1]]}
+
+#replicates = 3
+#max_steps = 100
+distinct_samples = 4 #N    N(D+2) rows output
+
+
+param_values = saltelli.sample(problem, distinct_samples, calc_second_order= False)
+#print(param_values)
+
+
+#count = 0
+samples = pd.DataFrame(data=param_values,
+                       columns=['cost_clean',
+                                'cost_dirty',
+                                'base_output_dirty',
+                                'base_output_clean',
+                                'metabolism_scalar_energy',
+                                'metabolism_scalar_money',
+                                'eta_global_trade',
+                                'predisposition_decrease'])
+
+
+samples.to_csv("Sobol_inputs.csv")
